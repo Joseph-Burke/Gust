a73a4f907a1d6436476f1dd38efbf0c27faf0032
Joseph Burke <joseph_burke@outlook.com>
2020-11-19 20:14:53 +0000

    Create fetchCityData function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ let body = document.querySelector('body');
 let test = document.createElement('p');
 test.textContent = 'Hey!';
 body.appendChild(test);
+
+function fetchCityData(cityName) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7a02375ad01cde29ee6c479e2e014776`, {mode: 'cors' })
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
