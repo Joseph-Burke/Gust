c34bb6b99180b9ad4d6b264d1f3c4ca82fe9f9bd
Joseph Burke <joseph_burke@outlook.com>
2020-11-24 15:15:17 +0000

    Install dependencies and configure webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-let body = document.querySelector('body');
-let test = document.createElement('p');
-test.textContent = 'Hey!';
-body.appendChild(test);
+import './styles/style.scss';
 
 function fetchCityData(cityName) {
   return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7a02375ad01cde29ee6c479e2e014776`, {mode: 'cors' })
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader', 
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
 };
