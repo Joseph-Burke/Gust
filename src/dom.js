78f56708f2c83629e5c0ea923401a80a99f7d161
Joseph Burke <joseph_burke@outlook.com>
2020-11-24 20:41:11 +0000

    Implement basic display of card w weather info

--- src/dom.js
+++ src/dom.js
@@ -18,8 +18,42 @@ const dom = {
   applyButtonEventListener: () => {
     const cityFormButton = document.getElementById('submit-city-form-button');
     cityFormButton.addEventListener('click', event => {
-      dom.submitCityForm(event.target.parentElement).then(object => console.log(object));
+      dom.submitCityForm(event.target.parentElement).then(data => dom.displayWeather(data));
     });
+  },
+  removePreviousDisplay: () => {
+    let display = document.getElementById("display-section");
+    if (display) {
+      display.parentNode.removeChild(display)
+    };
+  },
+  displayWeather: data => {
+    dom.removePreviousDisplay();
+    let formSection = document.getElementsByTagName('body')[0].children[0];
+    let display = helpers.createContent({
+      element: "section",
+      id: "display-section",
+      classList: ['display-section', 'container'],
+      children: [
+        {
+          element: "div",
+          classList: ["card", "card-body"],
+          children: [
+            {
+              element: "h3",
+              textContent: data.name,
+              classList: ['card-title']
+            },
+            {
+              element: "p",
+              textContent: data.weather[0].description,
+              classList: ['card-text']
+            }
+          ]
+        },
+      ]
+    });
+    formSection.insertAdjacentElement("afterend", display);
   },
   modifyDocument: () => {
     dom.applyButtonEventListener();
