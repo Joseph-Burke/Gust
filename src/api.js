490bbb5f9c11da171dc113ba8e7a34238f939214
Joseph Burke <joseph_burke@outlook.com>
2020-11-25 13:12:51 +0000

    Replace image fetch with color fetch

--- src/api.js
+++ src/api.js
@@ -3,16 +3,9 @@ const api = {
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7a02375ad01cde29ee6c479e2e014776`, { mode: "cors" })
     .then(response => response.json()),
 
-  fetchFlickrImage: searchTerm => {
-    return fetch(
-      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=423e997c88502153689ce5ae957e4cf9&text=${searchTerm + '%20weather%20sky'}&format=json&nojsoncallback=1`
-    )
-      .then(response => response.json())
-      .then(data => data.photos.photo[0])
-      .then(photoObj => {
-        const { server, id, secret, size } = photoObj;
-        return `https://live.staticflickr.com/${server}/${id}_${secret}.jpg`;
-      });
+  fetchColors: searchTerm => {
+    searchTerm = searchTerm.split(' ').join(',');
+    return fetch(`http://www.colr.org/json/tags/${searchTerm}`);
   }
 };
 
--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,9 @@
-import helpers from './helpers';
-import api from './api';
+import helpers from "./helpers";
+import api from "./api";
 
 const dom = {
   getFormInputValues: form => {
-    let inputs = form.getElementsByTagName('input');
+    let inputs = form.getElementsByTagName("input");
     inputs = Array.from(inputs);
     const inputValues = {};
     inputs.forEach(input => {
@@ -16,91 +16,106 @@ const dom = {
     return api.fetchCityData(formInfo.city);
   },
   applyButtonEventListener: () => {
-    const cityFormButton = document.getElementById('submit-city-form-button');
-    cityFormButton.addEventListener('click', event => {
+    const cityFormButton = document.getElementById("submit-city-form-button");
+    cityFormButton.addEventListener("click", event => {
       dom.submitCityForm(event.target.parentElement).then(data => {
         dom.displayWeather(data);
+        let weather = data.weather[0].main;
+        dom.changeBackground(weather);
       });
     });
   },
   applyCityInputEventListener: () => {
-    const cityInput = document.getElementById('city-input');
-    const cityFormButton = document.getElementById('submit-city-form-button');
-    cityInput.addEventListener('keypress', event => {
-    if (event.keyCode==13) {
-      event.preventDefault();
-      cityFormButton.click();
+    const cityInput = document.getElementById("city-input");
+    const cityFormButton = document.getElementById("submit-city-form-button");
+    cityInput.addEventListener("keypress", event => {
+      if (event.keyCode == 13) {
+        event.preventDefault();
+        cityFormButton.click();
       }
-    })
+    });
   },
   removePreviousDisplay: () => {
-    const display = document.getElementById('display-section');
+    const display = document.getElementById("display-section");
     if (display) {
       display.parentNode.removeChild(display);
     }
   },
   displayWeather: data => {
     dom.removePreviousDisplay();
-    const formSection = document.getElementsByTagName('body')[0].children[0];
+    const formSection = document.getElementsByTagName("body")[0].children[0];
     const display = helpers.createContent({
-      element: 'section',
-      id: 'display-section',
-      classList: ['display-section', 'container'],
+      element: "section",
+      id: "display-section",
+      classList: ["display-section", "container"],
       children: [
         {
-          element: 'div',
-          classList: ['card', 'my-3'],
+          element: "div",
+          classList: ["card", "my-3"],
           children: [
             {
-              element: 'div',
-              classList: ['row', 'no-gutters'],
+              element: "div",
+              classList: ["row", "no-gutters"],
               children: [
                 {
-                  element: 'div',
-                  classList: ['col-3'],
+                  element: "div",
+                  classList: ["col-3"],
                   children: [
                     {
-                      element: 'img',
+                      element: "img",
                       src: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
-                      classList: ['weather-icon', 'card-img'],
-                    },
-                  ],
+                      classList: ["weather-icon", "card-img"]
+                    }
+                  ]
                 },
                 {
-                  element: 'div',
-                  classList: ['col-9'],
+                  element: "div",
+                  classList: ["col-9"],
                   children: [
                     {
-                      element: 'div',
-                      classList: ['card-body'],
+                      element: "div",
+                      classList: ["card-body"],
                       children: [
                         {
-                          element: 'h3',
+                          element: "h3",
                           textContent: data.name,
-                          classList: ['card-title'],
+                          classList: ["card-title"]
                         },
                         {
-                          element: 'p',
+                          element: "p",
                           textContent: data.weather[0].description,
-                          classList: ['card-text'],
-                        },
-                      ],
-                    },
-
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
+                          classList: ["card-text"]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
     });
-    formSection.insertAdjacentElement('afterend', display);
+    formSection.insertAdjacentElement("afterend", display);
   },
   modifyDocument: () => {
     dom.applyButtonEventListener();
     dom.applyCityInputEventListener();
   },
+  setBodyBackground: color => {
+    let body = document.getElementById('body');
+    body.style['background-color'] = color;
+  },
+  changeBackground: searchTerm => {
+    searchTerm = searchTerm.toLowerCase();
+    api
+      .fetchColors(searchTerm)
+      .then(data => data.json())
+      .then(obj => {
+        let colour = obj.colors[0].hex;
+        dom.setBodyBackground(`#${colour}`);
+      });
+  }
 };
 
 export default dom;
